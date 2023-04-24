158f818adf259196ddb55a7d3fe1079974b1bc69
Eleazar Salazar <pepeslzr117@gmail.com>
2023-04-23 21:01:12 -0600

    a ver

--- src/main.jsx
+++ src/main.jsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
   },
   {
     path: "*", // handle all other routes with App component
-    element: <App />,
+    element: <Login />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
